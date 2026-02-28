27f31756b2e1ef35996a860bb8f75caf6df96d84
Chippo156 <vovannghiahiep150623@gmail.com>
2026-02-28 14:56:31 +0700

    first commit

---
+++ src/main/java/org/interview/cicd_githubaction_aws/CicdGithubActionAwsApplication.java
@@ -0,0 +1,13 @@
+package org.interview.cicd_githubaction_aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdGithubActionAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdGithubActionAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/interview/cicd_githubaction_aws/CicdGithubActionAwsApplicationTests.java
@@ -0,0 +1,16 @@
+package org.interview.cicd_githubaction_aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CicdGithubActionAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
